7c5656ccf973bc8e42eac067a71bad402a8efbf2
Adam Casey <adam.jw.casey@gmail.com>
2023-01-13 22:22:15 -0700

    Set up files with failing tests

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/// Finds the first occurence of 4 consecutive, different characters
+/// and returns the index of the last of those characters
+/// # Examples
+/// ```
+/// use advent_of_code_2022_06::find_packet_marker;
+/// assert_eq!(find_packet_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+/// assert_eq!(find_packet_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+/// assert_eq!(find_packet_marker("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+/// assert_eq!(find_packet_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+/// assert_eq!(find_packet_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+/// ```
+pub fn find_packet_marker(stream: &str) -> usize{
+    1
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use advent_of_code_2022_06::find_packet_marker;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = &env::args().collect::<Vec<_>>()[2];
+    let contents = fs::read_to_string(file_path)
+        .expect("Should be able to read the file");
+
+    println!("Found marker after receiving {} packets!", find_packet_marker(&contents));
 }
